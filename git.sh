90f2cd72e9f94e8788c865a8009030a88f5ee9f7
liangjieheng <ljh6c6a68@gmail.com>
2025-10-29 16:34:52 +0800

    init

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# bookmark-manager-privacy" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:playmaker86/bookmark-manager-privacy.git
+git push -u origin main
